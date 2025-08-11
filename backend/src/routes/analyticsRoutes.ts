0129b68619cda08e7ed5352812faf68cad35d267
Ayushd785 <ayushd785@gmail.com>
2025-08-11 15:19:56 +0530

    added routes for advanced analytics - getDetailedAnalytics and getRealTimeAnalytcs

--- backend/src/routes/analyticsRoutes.ts
+++ backend/src/routes/analyticsRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth";
-import { getDashboardStats, getLinkAnalytics } from "../controllers/analyticsController";
+import { getDashboardStats, getDetailedAnalytics, getLinkAnalytics, getRealTimeAnalytics } from "../controllers/analyticsController";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.use(authMiddleware);
 
 router.get("/dashboard",getDashboardStats);
 router.get("/link/:shortCode",getLinkAnalytics);
+router.get("/detailed",getDetailedAnalytics);
+router.get("/realtime",getRealTimeAnalytics);
 
 export default router;
